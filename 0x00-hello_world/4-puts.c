895a4324b5728f059e7807d18cc58a71f752b6f8
maryammusa225 <maryammusa225@gmail.com>
2022-03-10 16:28:44 -0800

    my commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - entry point
+*
+* puts -print declared char string to std output
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
