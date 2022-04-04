56457f98558c231acf360458d3e50e0f5306013e
maryammusa225 <83470772+maryammusa225@users.noreply.github.com>
2022-04-04 13:00:07 +0100

    Create holberton.h

---
+++ 0x0B-malloc_free/holberton.h
@@ -0,0 +1,9 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *);
+char *str_concat(char *s1, char *s2);
+
+#endif
